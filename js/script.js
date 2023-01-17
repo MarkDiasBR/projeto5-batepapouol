99d07e4d8a6d9d4afcfe873c46c43050479937e1
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-17 06:19:15 -0300

    Ajustes finos no Layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", e => {
+    textarea.style.height = "auto";
+    let scHeight = e.target.scrollHeight;
+    console.log(scHeight);
+    textarea.style.height = `${scHeight}px`;
+});
