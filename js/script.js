9c8252ba2ff0f68f0a2d81a27f0d9acd2e1d2d8f
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-19 18:34:40 -0300

    Data tests do avaliador configurado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//************Layout*************
+
 const textarea = document.querySelector("textarea");
 textarea.addEventListener("keyup", e => {
     textarea.style.height = "auto";
@@ -19,3 +21,38 @@ function toggleSidebar() {
 const overlayFora = document.querySelector(".overlay-fora");
 
 overlayFora.addEventListener("click",toggleSidebar);
+
+//************Axios*************
+
+function logarUsuario() {
+    const novoUsuario = {
+                            name: `${document.querySelector('.input-login').value}`
+                        };
+    alert(novoUsuario)
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', novoUsuario);
+    promessa.then(loginSucesso);
+    promessa.catch(loginFalha);
+}
+
+function loginSucesso(response) {
+    alert("sucesso");
+    const overlayEntrada = document.querySelector('.overlay-entrada');
+    overlayEntrada.classList.add("login-ok");
+    console.log(response);
+    
+}
+
+function loginFalha(response) {
+    alert("falha");
+    console.log(response);
+}
+
+function buscarMensagens() {
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', novoUsuario);
+    promessa.then(loginSucesso);
+    promessa.catch(loginFalha);
+}
+
+function mantemConexao() {
+
+}
