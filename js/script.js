179bc8fec714d44fb88f17dcc5c0c8f37788517f
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-21 20:12:15 -0300

    Favicon

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-//************Layout*************
-
-/*const textarea = document.querySelector("textarea");
-textarea.addEventListener("keyup", e => {
-    textarea.style.height = "auto";
-    let scHeight = e.target.scrollHeight;
-    console.log(scHeight);
-    textarea.style.height = `${scHeight}px`;
-});
-*/
 function toggleSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("ativo");
